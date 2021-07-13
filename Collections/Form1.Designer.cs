3bf0cc65ad60a50d1e9e9d0dbb993bcd97a6675e
maenstru56 <peteleaza.darius@yahoo.com>
2021-07-13 15:23:34 +0300

    Add project files.

---
+++ Collections/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Collections
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(498, 794);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(176, 86);
+            this.testButton.TabIndex = 0;
+            this.testButton.Text = "Test";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1388, 999);
+            this.Controls.Add(this.testButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testButton;
+    }
+}
+
---
+++ Collections/GenericClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class GenericClass<Tip>
+    {
+        Tip a;
+        Tip b;
+
+        public GenericClass(Tip a, Tip b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public double Sum()
+        {
+            return Convert.ToDouble(a) + Convert.ToDouble(b);
+        }
+    }
+}
